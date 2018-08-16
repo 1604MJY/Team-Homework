7644c14a31dd382a7e6e415258fb4b111c60294a
unknown <313511528@qq.com>
2018-08-16 11:53:19 +0800

    second

---
+++ 电路计算修改二/WindowsFormsApp2/cacu2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class Cacu2
+    {
+        static public double Ca2(Circuit circuit)
+        { double Resisnum,current;
+            Resisnum = (circuit.Resis2 * circuit.Resis4) / (circuit.Resis2 + circuit.Resis4);
+            current = Math.Round((circuit.Voltage / (circuit.Resis1 + Resisnum)) + (circuit.Voltage / circuit.Resis3),3);
+            return current;
+        }
+    }
+}
